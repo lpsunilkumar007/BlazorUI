2d01d6096bfc38d2eb2e3cd1aa3efa20ecb9c369
mukesh <mukeshsalaria01@gmail.com>
2024-01-24 19:06:49 +0530

    Adding mudblazor package

--- Portal/Portal.UI/Program.cs
+++ Portal/Portal.UI/Program.cs
@@ -1,5 +1,8 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
 using Portal.UI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +10,15 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage()
+                .AddMudServices(configuration =>
+                {
+                    configuration.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+                    configuration.SnackbarConfiguration.HideTransitionDuration = 100;
+                    configuration.SnackbarConfiguration.ShowTransitionDuration = 100;
+                    configuration.SnackbarConfiguration.VisibleStateDuration = 3000;
+                    configuration.SnackbarConfiguration.ShowCloseIcon = false;
+                });
+
 
 await builder.Build().RunAsync();

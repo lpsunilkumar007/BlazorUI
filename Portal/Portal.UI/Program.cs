541e77adc12cd2addb63152f3d2f373c3102ad6f
mukesh <mukeshsalaria01@gmail.com>
2024-01-24 19:34:48 +0530

    setting up Login form

--- Portal/Portal.UI/Program.cs
+++ Portal/Portal.UI/Program.cs
@@ -1,8 +1,10 @@
 using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor;
 using MudBlazor.Services;
+using Portal.Services.AuthProviders;
 using Portal.UI;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -10,6 +12,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredLocalStorage()
                 .AddMudServices(configuration =>
                 {
@@ -19,6 +23,6 @@ builder.Services.AddBlazoredLocalStorage()
                     configuration.SnackbarConfiguration.VisibleStateDuration = 3000;
                     configuration.SnackbarConfiguration.ShowCloseIcon = false;
                 });
-
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 
 await builder.Build().RunAsync();

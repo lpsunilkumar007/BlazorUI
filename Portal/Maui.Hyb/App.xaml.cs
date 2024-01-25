a03e20bed1253e74d08affa0ff4b4b820fd1f192
mukesh <mukeshsalaria01@gmail.com>
2024-01-25 19:23:16 +0530

    upgrading to .net 7

---
+++ Portal/Maui.Hyb/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui.Hyb
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Portal/Maui.Shared/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Maui.Shared
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
